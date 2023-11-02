ebbd01acddd60295a5db5b84a3d285ed6e4f58f2
Lisa Patricia Kelly <lisa.kelly1@MDS.ac.nz>
2023-11-02 21:53:03 +1300

    +World flip can go back

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -15,12 +15,15 @@ public class GameController : MonoBehaviour
 {
     // variables.
     XPBar ExpBar;
+    GrayScale MakeGray;
+    CameraShake ShakeCam;
 
     GameState m_State;
     [SerializeField] PlayerController m_PlayerController;
     [SerializeField] BattleSystem m_BattleSystem;
     [SerializeField] Camera m_WorldCamera;
     [SerializeField] XPBar m_XPBar;
+    [SerializeField] CameraShake m_CameraShake;
     [SerializeField] AudioManager m_AudioManager;
 
     // Subscribe to the created event
@@ -29,7 +32,8 @@ public class GameController : MonoBehaviour
         // Observer/Subscriber Pattern
         m_PlayerController.OnEncountered += StartBattle;
         m_BattleSystem.OnBattleOver += EndBattle;
-        m_XPBar.OnXPNotch += StartWowrldFlip; 
+        m_XPBar.OnXPNotch += StartWorldFlip;
+        m_CameraShake.OnShakeOver += EndWorldFlip;
     }
 
     void StartBattle()
@@ -56,7 +60,15 @@ public class GameController : MonoBehaviour
         m_XPBar.UpdateProgress(_xp);
         m_AudioManager.SetMusic(GameState.FreeRoam);
     }
-    void StartWowrldFlip()
+    public void EndWorldFlip()
+    {
+        m_State = GameState.FreeRoam;
+        m_BattleSystem.gameObject.SetActive(false);
+        m_WorldCamera.gameObject.SetActive(true);
+        m_XPBar.gameObject.SetActive(true);
+        m_AudioManager.SetMusic(GameState.FreeRoam);
+    }
+    void StartWorldFlip()
     {
         Debug.Log("World Flip triggered");
 
@@ -66,11 +78,13 @@ public class GameController : MonoBehaviour
         m_State = GameState.WorldFlip;
         m_BattleSystem.gameObject.SetActive(false);
         m_WorldCamera.gameObject.SetActive(true);
-        m_XPBar.gameObject.SetActive(true);
+        m_XPBar.gameObject.SetActive(false);
 
         //// Change Music
         //m_BattleSystem.Begin();
         m_AudioManager.SetMusic(GameState.WorldFlip);
+
+        CameraShake.Instance.ShakeCamera(1.0f, 20.0f);
     }
 
 
